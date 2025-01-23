7a14006027e816c44d754844cbba8955c1e607bc
jiyeonbeack <jiyeonbeack@gmail.com>
2025-01-23 15:16:37 +0900

    1st commit
    첫번째 커밋입니다.

---
+++ tips_load.py
@@ -0,0 +1,12 @@
+import seaborn as sns
+
+def get_tips():
+    tips = sns.load_dataset("tips")
+    return tips
+
+def print_get():
+    print("테스트 확인")
+    return "일어나세요 용사여"
+
+if __name__ == "__main__":
+    print_get()
